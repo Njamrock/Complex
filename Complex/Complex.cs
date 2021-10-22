348252135eb3e285c575f88a2769572dcd26cf41
Pavle Pavicic <PavleP@dspace.hr>
2021-10-22 10:26:26 +0200

    Completed Zadatak3.

--- Complex/Complex.cs
+++ Complex/Complex.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Complex
 {
@@ -27,6 +25,32 @@ namespace Complex
         public static Complex operator+(Complex a, Complex b)
         {
             return new Complex(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+
+        public static Complex operator-(Complex a, Complex b)
+        {
+            return new Complex(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+
+        public static Complex operator*(Complex a, Complex b)
+        {
+            return new Complex(a.Real*b.Real - a.Imaginary*b.Imaginary, a.Real*b.Imaginary + a.Imaginary*b.Real);
+        }
+
+        public static Complex operator/(Complex a, Complex b)
+        {
+            return new Complex((a.Real * b.Real + a.Imaginary * b.Imaginary)/(b.real*b.real + b.Imaginary*b.imaginary), (a.Imaginary * b.Real - a.Real * b.Imaginary) / (b.real * b.real + b.Imaginary * b.imaginary));
+        }
+
+        public static Complex[] Sqrt(double number)
+        {
+            return number >= 0 ? new Complex[] { new Complex(Math.Sqrt(number), 0), new Complex(-Math.Sqrt(number), 0) } : new Complex[] { new Complex(0, Math.Sqrt(-number)), new Complex(0, -Math.Sqrt(-number)) };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", this.real, this.Imaginary);
+
         }
 
         private double real;
--- Complex/Program.cs
+++ Complex/Program.cs
@@ -9,11 +9,35 @@ namespace Complex
             Complex a = new Complex(1, 2);
             Complex b = new Complex(3, 4);
             Complex c = a + b;
+            Console.WriteLine($"Addition: ({a.Real} + {a.Imaginary}*i) + ({b.Real} + {b.Imaginary}*i) = {c.Real} + {c.Imaginary}*i");
+            Console.WriteLine($"Addition: ({a.ToString()}) + ({b.ToString()}) = {c.ToString()}\n");
 
-            Console.WriteLine(c.Real);
-            Console.WriteLine(c.Imaginary);
+            a = new Complex(3, 4);
+            b = new Complex(2, 1);
+            Complex d = a - b;
+            Console.WriteLine($"Subtraction: ({a.Real} + {a.Imaginary}*i) - ({b.Real} + {b.Imaginary}*i) = {d.Real} + {d.Imaginary}*i");
+            Console.WriteLine($"Subtraction: ({a.ToString()}) - ({b.ToString()}) = {d.ToString()}\n");
 
-            // TODO:01 Finish Lab2 Zadatak 3.)
+
+            a = new Complex(1, 2);
+            b = new Complex(3, 4);
+            Complex e = a * b;
+            Console.WriteLine($"Multiplication: ({a.Real} + {a.Imaginary}*i) * ({b.Real} + {b.Imaginary}*i) = {e.Real} + {e.Imaginary}*i");
+            Console.WriteLine($"Multiplication: ({a.ToString()}) * ({b.ToString()}) = {e.ToString()}\n");
+
+            a = new Complex(5, 5);
+            b = new Complex(1, 2);
+            Complex f = a / b;
+            Console.WriteLine($"Division: ({a.Real} + {a.Imaginary}*i) / ({b.Real} + {b.Imaginary}*i) = {f.Real} + {f.Imaginary}*i");
+            Console.WriteLine($"Division: ({a.ToString()}) / ({b.ToString()}) = {f.ToString()}\n");
+
+            double g = 4;
+            Console.WriteLine($"Roots of number {g} are: {Complex.Sqrt(g)[0].Real} + {Complex.Sqrt(g)[0].Imaginary}*i , {Complex.Sqrt(g)[1].Real} + {Complex.Sqrt(g)[1].Imaginary}*i");
+            Console.WriteLine($"Roots of number {g} are: {Complex.Sqrt(g)[0].ToString()} , {Complex.Sqrt(g)[1].ToString()}\n");
+
+            g = -4;
+            Console.WriteLine($"Roots of number {g} are: {Complex.Sqrt(g)[0].Real} + {Complex.Sqrt(g)[0].Imaginary}*i , {Complex.Sqrt(g)[1].Real} + {Complex.Sqrt(g)[1].Imaginary}*i");
+            Console.WriteLine($"Roots of number {g} are: {Complex.Sqrt(g)[0].ToString()} , {Complex.Sqrt(g)[1].ToString()}\n");
         }
     }
 }
